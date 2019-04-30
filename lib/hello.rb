d13c1b6c0720f7303876bf0e8333090c60236a3d
Tanuka Das <das.tanuka@yahoo.com>
2019-04-30 02:06:47 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,13 @@
-def hello_t
-
+def hello_t(array)
+  # call your method here!
+  if block_given?
+  i = 0
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+  array                     #returns the original array
+else
+  puts "Hey! No block was given!"
 end
-
-# call your method here!
-
+end
